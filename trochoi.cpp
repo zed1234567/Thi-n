c0c1f5d80da8f225cb857fc3163cbd0813277023
zed1234567 <40890889+zed1234567@users.noreply.github.com>
2018-07-24 21:06:31 +0700

    ĐIỀU CHỈNH

--- trochoi.cpp
+++ trochoi.cpp
@@ -2,27 +2,37 @@
 #include<stdlib.h>
 #include<time.h>
 int main(){
-	int soBiMat=0,soHienTai=0,i=0,dk,mode;
+	int soBiMat=0,
+	    soHienTai=0,
+	    i=0,dk,mode;
 	const int Max=100,Min=1;
 	srand(time(NULL));
 //	soBiMat=(rand()%(Max-Min+1)+Min);
+//      TRÒ CHƠI ĐOÁN SỐ BÍ MẬT	
 	printf("******MENU*****\n");
-	printf("Xin chon che do choi 1 nguoi or 2 nguoi: ");scanf("%d",&mode);
-	switch(mode){
+	printf("Xin chon che do choi 1 nguoi or 2 nguoi: ");
+	scanf("%d",&mode);
+	switch(mode)
+	{
 		case 1:
 			{
 			soBiMat=(rand()%(Max-Min+1)+Min);
  do{
 	do{
-		printf("Moi ban nhap so doan: ");scanf("%d",&soHienTai);
-		if(soBiMat>soHienTai){
+		printf("Moi ban nhap so doan: ");
+		scanf("%d",&soHienTai);
+		if(soBiMat>soHienTai)
+		{
 			printf("So lon hon.\n");
-			i++;}
-		else if(soBiMat<soHienTai){
-				printf("So nho hon.\n");
-				i++;}
+			i++;
+		}
+		else if(soBiMat<soHienTai)
+		{
+			printf("So nho hon.\n");
+			i++;
+		}
 		else
-					printf("chung mung ban da doan dung sau %d lan.\n",i+1);
+			printf("chung mung ban da doan dung sau %d lan.\n",i+1);
 	}while(soBiMat!=soHienTai);
 	printf("Ban co muon tiep tuc khong.(1 tiep tuc 0 thoat): ");
 	scanf("%d",&dk);
@@ -35,16 +45,20 @@ case 2:{
 	scanf("%d",&soBiMat);
 do{
 	do{
-		
-		printf("\nMoi nguoi choi thu hai nhap so du doan: ");scanf("%d",&soHienTai);
-		if(soBiMat>soHienTai){
+		printf("\nMoi nguoi choi thu hai nhap so du doan: ");
+		scanf("%d",&soHienTai);
+		if(soBiMat>soHienTai)
+		{
 			printf("So lon hon va moi nguoi choi thu hai nhap lai.\n");
-			i++;}
-		else if(soBiMat<soHienTai){
-				printf("So nho hon va moi nguoi choi thu hai nhap lai.\n");
-				i++;}
+			i++;
+		}
+		else if(soBiMat<soHienTai)
+		{
+		        printf("So nho hon va moi nguoi choi thu hai nhap lai.\n");
+			i++;
+		}
 		else
-					printf("chung mung ban da doan dung sau %d lan.\n",i+1);
+			printf("chung mung ban da doan dung sau %d lan.\n",i+1);
 	}while(soBiMat!=soHienTai);
 	printf("Ban co muon tiep tuc khong.(1 tiep tuc 0 thoat): ");
 	scanf("%d",&dk);
